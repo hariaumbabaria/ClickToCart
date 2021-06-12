9b19b7a8a7d0cf86b029ee36856b7c7dc372d154
hariaumbabaria <hariaumbabaria@gmail.com>
2021-06-12 15:07:02 +0530

    1

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+//components
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
