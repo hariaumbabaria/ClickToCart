123d2bc43f01dd8025d610b2d42e61a64d66d293
hariaumbabaria <hariaumbabaria@gmail.com>
2021-06-14 15:07:16 +0530

    7

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,22 +1,60 @@
+import { Box, makeStyles } from '@material-ui/core';
 
+//component
 import NavBar from './NavBar'
 import Banner from './Banner';
-import { Box, makeStyles } from '@material-ui/core';
+import Slide from './Slide';
+import MidSection from './MidSection';
 
 const useStyle = makeStyles ({
     component: {
         padding: 10,
         background: '#f2f2f2'
+    },
+    rightWrapper: {
+        background: '#ffffff',
+        padding: 5,
+        margin: '12px 0 0 10px', 
+        width: '17%'
     }
 })
 
 const Home = () => {
     const classes = useStyle();
+    const adURL = 'https://rukminim1.flixcart.com/flap/464/708/image/633789f7def60050.jpg?q=70';
     return (
         <div>
             <NavBar />
             <Box className = {classes.component}>
                 <Banner />
+                <Box style={{display: 'flex'}}>
+                    <Box style={{width: '83%'}}>
+                        <Slide 
+                            timer={true}
+                            title="Deal of the Day"
+                        />
+                    </Box>
+                    <Box className = {classes.rightWrapper}>
+                        <img src={adURL} style={{width:230}} />
+                    </Box>
+                </Box>
+                <MidSection />
+                <Slide 
+                    timer = {false} 
+                    title="Discounts for You"
+                />
+                <Slide 
+                    timer = {false} 
+                    title="Suggested Items"
+                />
+                <Slide 
+                    timer = {false} 
+                    title="Top Selection"
+                />
+                <Slide 
+                    timer = {false} 
+                    title="Bestsellers"
+                />
             </Box>
         </div>
     )
